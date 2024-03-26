6c1ea8bc09a0aaa5c51e5ca3d3a23dc14a85ed68
Purvesh Krishnani <purvesh152001@gmail.com>
2024-03-26 16:26:31 +0530

    .

---
+++ PatientRegistrationService/PatientRegistrationService/src/test/java/com/ClinicManagementSystem/PatientRegistrationService/PatientRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClinicManagementSystem.PatientRegistrationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
