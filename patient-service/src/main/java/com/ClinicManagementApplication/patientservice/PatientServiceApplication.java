29eb2928124630fb9743f62748c7902461245540
Purvesh Krishnani <purvesh152001@gmail.com>
2024-05-14 04:39:30 +0530

    .

--- patient-service/src/main/java/com/ClinicManagementApplication/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/ClinicManagementApplication/patientservice/PatientServiceApplication.java
@@ -3,8 +3,10 @@ package com.ClinicManagementApplication.patientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
